bcdae6c45a16f3d8407a70e8dd2fb732e29b493e
jandrewgordon <jandrewgordon@gmail.com>
2021-05-13 11:51:17 +0100

    finished football tests

--- football_results/src/football_results.py
+++ football_results/src/football_results.py
@@ -1,6 +1,23 @@
-def get_result(final_score):
-    pass
+def get_result(final_scores):
+    if final_scores["home_score"] > final_scores["away_score"]:
+        return "Home win"
+    elif final_scores["away_score"] > final_scores["home_score"]:
+        return "Away win"
+    else:
+        return "Draw"
 
 def get_results(final_scores):
-    pass
+    scores_list = []
+    for score in final_scores:
+        scores_list.append(get_result(score))
+    return scores_list
+
+    
+
+
+    
     # (You could try and use a list comprehension for this)
+
+
+
+    
--- football_results/tests/football_results_test.py
+++ football_results/tests/football_results_test.py
@@ -2,7 +2,29 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.final_scores = [
+            {"home_score": 0, "away_score": 1},
+            {"home_score": 1, "away_score": 0},
+            {"home_score": 1, "away_score": 1}   
+        ]
+    
+    def test_greater_away_score_returns__away_win(self):
+        self.assertEqual("Away win", get_result(self.final_scores[0]))
+
+    def test_greater_home_score_returns__home_win(self):
+        self.assertEqual("Home win", get_result(self.final_scores[1]))
+
+    def test_equal_scores_returns__draw(self):
+        self.assertEqual("Draw", get_result(self.final_scores[2]))
+
+    def test_get_results__returns_list_of_results(self):
+        self.assertEqual(3, len(get_results(self.final_scores)))
+    
+
+
+
     # Test we get the right result string for a final score dictionary representing -
 
         # Home win
